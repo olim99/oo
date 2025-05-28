3f3801352dc52943ae2f287dba3f2d913f766eb4
Olimjon <olimjon@example.com>
2025-05-28 18:23:12 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/aa/',
+  base: '/оо/', // должно быть точно так же, как название репозитория на GitHub
   plugins: [react()]
 })
